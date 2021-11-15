894146275e802af942fb99d6a2613e91a1c807e1
Manuel Rodríguez Carmona <mrodcar634@g.educaand.es>
2021-11-15 17:06:38 +0100

    Longitud2.0

---
+++ Prog3_0.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Prog3_0 {
+	
+	static Scanner entrada= new Scanner(System.in);
+    public static void main(String[] args) {
+    	
+    	double longitud;
+    	double radio = entrada.nextDouble();
+    	
+    	 longitud = 2 * (Math.PI)* radio;
+    }
+}
