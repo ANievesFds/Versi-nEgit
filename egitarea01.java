d22eb0c034e00ba0f07af2073ca9976bcc99f589
Alicia Nieves <alinievesfds26@gmail.com>
2021-11-15 17:11:33 +0100

    Versión2Area01

---
+++ egitarea01.java
@@ -0,0 +1,14 @@
+
+public class egitarea01 {
+
+	public static void main(String[] args) {
+		
+		double area;
+		double radio=0;
+		   area = (Math.PI) * (Math.pow(radio, 2));
+		   System.out.println("El área de la esfera cuyo radio " + radio + "es" + area);
+		System.out.println("Finalizado Version 2");
+
+	}
+
+}
