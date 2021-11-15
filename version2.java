85d3ef69848f2d070bbb828a717eed4815305cd3
JJulio95 <jcammar773@g.educaand.es>
2021-11-15 18:25:41 +0100

    version2

--- version2.java
+++ version2.java
@@ -3,13 +3,14 @@ package Prog100;
 import java.util.Scanner;
 import java.lang.Math;
 
-public class Version1 {
+public class version2 {
 
 	public static void main(String[] args) {
 	
 		Scanner leerradio = new Scanner(System.in);
 		int radio;
-		
+		double volumen;
+
 
 		
 		System.out.println("Introduce el radio de la esfera");
