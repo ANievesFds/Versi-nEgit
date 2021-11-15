b2c90b61cfc21214ba3b60c9f39fc7cec628f5d8
JJulio95 <jcammar773@g.educaand.es>
2021-11-15 17:24:46 +0100

    version1.0

---
+++ Version1.java
@@ -0,0 +1,16 @@
+package Prog100;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Version1 {
+
+	public static void main(String[] args) {
+	
+		Scanner leerradio = new Scanner(System.in);
+		int radio;
+		System.out.println("Introduce el radio de la esfera");
+		 radio=leerradio.nextInt();
+		}
+	}
+
