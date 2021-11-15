de9f9841720802c5cd16ba082eb8d12814ce8476
jdelalo920 <jdelalo920@g.educaand.es>
2021-11-15 17:03:19 +0100

    versión2area2

---
+++ egitarea02.java
@@ -0,0 +1,13 @@
+
+public class egitarea02 {
+
+	public static void main(String[] args) {
+		double radio=0;
+		double area;
+		 area = (Math.PI) * (Math.pow(radio, 2));
+		 System.out.println("El área de la esfera cuyo radio "+ radio +" es"+area);
+		System.out.println("Finalizado Version 2");
+
+	}
+
+}
