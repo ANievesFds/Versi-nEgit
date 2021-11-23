30870b5d6fba2deabc8b09bbc9986beef249c6f7
alecabot <alejandrocbt@hotmail.com>
2021-11-23 10:41:39 +0100

    version3

--- src/proyecto01/prog_03.java
+++ src/proyecto01/prog_03.java
@@ -2,15 +2,14 @@ package proyecto01;
 
 import java.util.Scanner;
 
-public class prog_02 {
+public class prog_03 {
 
 	public static void main(String[] args) {
 		double volumen;
 		Scanner leerradio = new Scanner(System.in);
-		int radio;
+		int radio = 0;
 		System.out.println("Introduce el radio de la esfera");
-		}
-
+		volumen = (4 * (Math.PI) * (Math.pow(radio,3))/3);
 	}
 
-
+}
